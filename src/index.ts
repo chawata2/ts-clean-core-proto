7830db4ee783cb05dbe293fdc7e3875cfa43f2ac
chawaRA <cha39881@gmail.com>
2025-07-06 16:33:46 +0900

    feat: User api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
+import z from "zod";
+import { createUserUC } from "./useCases/createUserUC";
+import { inMemoryUserRepo } from "./useCases/inMemoryUserRepo";
+
+const createUserSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+});
+
 const server = Bun.serve({
   port: 3000,
+  routes: {
+    "/api/users": {
+      GET: () => Response.json({ users: [] }),
+      POST: async (req) => {
+        const body = await req.json();
+        const parsed = createUserSchema.safeParse(body);
+        if (!parsed.success) {
+          return Response.json(
+            {
+              message: "Invalid request body",
+              errors: parsed.error,
+            },
+            { status: 422 },
+          );
+        }
+        return Response.json(await createUserUC(parsed.data, inMemoryUserRepo));
+      },
+    },
+  },
   fetch(req) {
-    return new Response("Bun!");
+    return new Response(`Not Fountd`, { status: 404 });
   },
 });
 
